924aaab98fe41deee3139cfb6ac58937adf4de11
K S Prateek <ksprateek2004@gmail.com>
2025-11-06 16:02:36 +0000

    testing changes

---
+++ NEW-KEDA-based-implementatioon/keda/generate-sleepable-dependent-microservices.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+mkdir -p keda-scaledobjects
+
+BACKENDS=("adservice" "cartservice" "checkoutservice" "currencyservice" "emailservice" "loadgenerator" "paymentservice" "productcatalogservice" "recommendationservice" "redis-cart" "shippingservice")
+
+for svc in "${BACKENDS[@]}"; do
+cat <<EOF > keda-scaledobjects/${svc}-scaledobject.yaml
+apiVersion: keda.sh/v1alpha1
+kind: ScaledObject
+metadata:
+  name: ${svc}-scaledobject
+spec:
+  scaleTargetRef:
+    name: ${svc}
+  pollingInterval: 15
+  cooldownPeriod: 30
+  minReplicaCount: 0
+  maxReplicaCount: 1
+  triggers:
+  - type: prometheus
+    metadata:
+      serverAddress: http://prometheus-kube-prometheus-prometheus.monitoring.svc.cluster.local:9090
+      metricName: frontend_up
+      query: kube_deployment_status_replicas{deployment="frontend",namespace="default"}
+      threshold: '1'
+EOF
+echo "Generated ScaledObject for ${svc} -> ./keda-scaledobjects/${svc}-scaledobject.yaml"
+done
